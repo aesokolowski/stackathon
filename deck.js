575ac17ee18943b8803f533e60817f6e7c2c8c9a
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2019-11-15 11:54:47 -0500

    algo: deck can be generated and shuffled

--- deck.js
+++ deck.js
@@ -1,5 +1,66 @@
 const card = require('./card');
+const { CLUBS, DIAMONDS, HEARTS, SPADES } = require('./enums/suitEnums');
+const { BLACK, RED } = require('./enums/colorEnums');
+const { ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN,
+        KING, JOKER } = require('./enums/nameEnums');
+
+const DEF_AH = true,
+      DEF_WD = [],
+      DEF_LB = false,
+      DEF_JK = false;
 
 class deck {
+  constructor(options = {}) {
+    this.aceHigh = typeof options.aceHigh === 'undefined' ?
+      DEF_AH : options.aceHigh;
+    this.wild = options.wild || DEF_WD;
+    this.lowball = typeof options.lowball === 'undefined' ?
+      DEF_LB : options.lowBall;
+    this.joker = typeof options.jokers === 'undefined' ?
+      DEF_JK : options.jokers;
+    this.trumps = options.trumps || null;
+    this.draw = deck.generateDeck({
+      aceHigh: this.aceHigh,
+      wild: this.wild,
+      lowball: this.lowball,
+      joker: this.joker,
+      trumps: this.trumps
+    });
+    this.inPlay = [];
+    this.discard = [];
+  }
 
+  // class function
+  static generateDeck(options) {
+    const deck = [];
+
+    // loop through enumerated
+    if (!this.jokers) {
+      for (let i = KING; i >= ACE; i--) {
+        for (let j = SPADES; j >= CLUBS; j--) {
+          deck.push(new card(
+            i, j, i === ACE ?
+              options.aceHigh ? 14 : 1
+            : i
+          ));
+        }
+      }
+    // don't need jokers for any games at this point
+    } else {
+      throw 'Jokers not implemented yet';
+    }
+
+    return deck;
+  }
+
+  // Fisher-Yates algorithm, from pseudocode on Wikipedia
+  shuffle() {
+    for (let i = this.draw.length - 1; i >= 1; i--) {
+      let j = Math.floor(Math.random() * i);
+
+      [this.draw[j], this.draw[i]] = [this.draw[i], this.draw[j]];
+    }
+  }
 }
+
+module.exports = deck;
---
+++ enums/suitEnums.js
@@ -0,0 +1,14 @@
+// since 0 is falsey in javascript, it seems like a good idea to start with 1
+// when dealing with this language that technically doesn't have enums
+
+const CLUBS = 1,
+      DIAMONDS = 2,
+      HEARTS = 3,
+      SPADES = 4;
+
+module.exports = {
+  CLUBS,
+  DIAMONDS,
+  HEARTS,
+  SPADES
+};
